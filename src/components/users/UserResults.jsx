25b0646116e237ba8445f9e55abe495f21fe9607
Rafal Ochyra <rochyra@primebitgames.com>
2022-01-15 16:20:43 +0100

    DIsplay users

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+
+function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await response.json();
+
+    setUsers(data);
+    setLoading(false);
+  };
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cold-3 md:grid-cols-2">
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
+}
+
+export default UserResults;
